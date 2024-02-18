748f37edf7de3843c0bdc6e313ad8ee820149ca0
salsabilatuadaa <salsabilatuadaa7@gmail.com>
2024-02-18 12:04:25 +0700

    benerin 404 not found

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 
 */
 
 
@@ -89,6 +89,6 @@ Route::group(['middleware' => 'guest'], function () {
 
 });
 
-Route::get('/login', function () {
+Route::get('/', function () {
     return view('session/login-session');
 })->name('login');
