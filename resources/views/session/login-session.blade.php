fb8ce380f75cebc66676a023f1d1ecfe8e4aad09
Ajeng <ajengnarr@gmail.com>
2024-03-20 12:27:46 +0700

    Mengubah tampilan login

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -10,12 +10,10 @@
             <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
               <div class="card card-plain mt-8">
                 <div class="card-header pb-0 text-left bg-transparent">
-                  <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
-                  <p class="mb-0">Create a new acount<br></p>
-                  <p class="mb-0">OR Sign in with these credentials:</p>
-                  <p class="mb-0">Email <b>admin@softui.com</b></p>
-                  <p class="mb-0">Password <b>secret</b></p>
+                  <h3 class="font-weight-bolder text-danger">Sistem Cuti Online</h3>
+                  <h3 class="font-weight-bolder text-danger">Diskominfo Kota Semarang</h3>
                 </div>
+
                 <div class="card-body">
                   <form role="form" method="POST" action="/session">
                     @csrf
@@ -38,24 +36,24 @@
                       <label class="form-check-label" for="rememberMe">Remember me</label>
                     </div>
                     <div class="text-center">
-                      <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+                      <button type="submit" class="btn bg-danger text-white w-100 mt-4 mb-0">Sign in</button>
                     </div>
                   </form>
                 </div>
                 <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                <small class="text-muted">Forgot you password? Reset you password 
-                  <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
+                <small class="text-muted">Forgot your password? Reset your password 
+                  <a href="/login/forgot-password" class="text-danger font-weight-bold">here</a>
                 </small>
                   <p class="mb-4 text-sm mx-auto">
                     Don't have an account?
-                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
+                    <a href="register" class="text-danger font-weight-bold">Sign up</a>
                   </p>
                 </div>
               </div>
             </div>
             <div class="col-md-6">
-              <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
-                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/curved-images/curved6.jpg')"></div>
+              <div class="oblique position-absolute top-0 h-100 d-md-block d-none">
+                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0" style="background-image:url('../assets/img/Diskominfo.jpeg'); background-color: red;"></div>
               </div>
             </div>
           </div>
